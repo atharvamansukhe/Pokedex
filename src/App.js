51b53cca13120888b9836ce4f1f20796b058abe9
nohero77 <atharvamansukhe17@gmail.com>
2021-11-13 20:14:56 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InfoDialog from "./components/InfoDialog";
 import axios from "axios";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import Scroll from "./components/Scroll";
+import Footer from "./components/Footer";
 
 class App extends React.Component {
   constructor(props) {
@@ -609,38 +610,15 @@ class App extends React.Component {
                 No such Pokémon in this region :/
               </div>
             )}
-            <div className="app__footer noselect">
-              <div>
-                Built using{" "}
-                <a
-                  href="https://pokeapi.co/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  PokéAPI
-                </a>
-                ,{" "}
-                <a
-                  href="https://reactjs.org/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  React
-                </a>{" "}
-                and{" "}
-                <a
-                  href="https://material-ui.com/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Material-UI
-                </a>
-                .
-              </div>
-              <div onClick={this.openGithub} className="github__icon">
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </div>
+            {/* <div className="app__footer noselect">
+                        <div>
+                            Built using <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokéAPI</a>, <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://material-ui.com/" target="_blank" rel="noopener noreferrer">Material-UI</a>.
+                        </div>
+                        <div onClick={this.openGithub} className="github__icon">
+                            <GitHubIcon></GitHubIcon>
+                        </div>
+                    </div> */}
+            <Footer />
           </div>
         )}
       </>
