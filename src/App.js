71b284ba22fa95a4d66e5e6192ece559490010bc
nohero77 <atharvamansukhe17@gmail.com>
2021-11-13 20:16:07 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InfoDialog from "./components/InfoDialog";
 import axios from "axios";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import Scroll from "./components/Scroll";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 class App extends React.Component {
@@ -379,28 +380,29 @@ class App extends React.Component {
     window.open("https://github.com/s1varam/pokedex");
   };
 
-  changeTheme = () => {
-    // debugger
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    // console.log(currentTheme);
+  // changeTheme = () => {
 
-    var targetTheme = "light";
+  //     // debugger
+  //     var currentTheme = document.documentElement.getAttribute('data-theme');
+  //     // console.log(currentTheme);
 
-    if (currentTheme === "light") {
-      targetTheme = "dark";
+  //     var targetTheme = "light";
 
-      this.setState({
-        isChecked: true,
-      });
+  //     if (currentTheme === "light") {
+  //         targetTheme = "dark";
 
-      // console.log(targetTheme);
-    } else {
-      this.setState({
-        isChecked: false,
-      });
-    }
-    document.documentElement.setAttribute("data-theme", targetTheme);
-  };
+  //         this.setState({
+  //             isChecked: true,
+  //         })
+
+  //         // console.log(targetTheme);
+  //     } else {
+  //         this.setState({
+  //             isChecked: false,
+  //         })
+  //     }
+  //     document.documentElement.setAttribute('data-theme', targetTheme)
+  // }
 
   handleClick = () => {
     window[`scrollTo`]({ top: document.body.scrollHeight, behavior: `smooth` });
@@ -441,34 +443,26 @@ class App extends React.Component {
                 evolutionPokemon={this.fetchPokemonData}
               ></InfoDialog>
             )}
-            <div className="app__header">
-              <div className="switch">
-                <div className="toggle">
-                  <label htmlFor="themeSwitch"></label>
-                  <input
-                    type="checkbox"
-                    name="swich-theme"
-                    id="themeSwitch"
-                    onClick={this.changeTheme}
-                    defaultChecked
-                  />
-                  <div className="toggle-bg"></div>
-                  <div className="toggle-thumb">
-                    <i className="fas fa-sun"></i>
-                    <i className="fas fa-moon"></i>
-                  </div>
-                </div>
-              </div>
-              <div className="poke__logos noselect">
-                <img src={Pokedex} alt="pokelogo" className="poke__logo" />
-              </div>
-              <div
-                className="pokeball__box github__icon"
-                onClick={this.openGithub}
-              >
-                <GitHubIcon></GitHubIcon>
-              </div>
-            </div>
+            {/* <div className="app__header">
+                        <div className="switch">
+                            <div className="toggle">
+                                <label htmlFor="themeSwitch"></label>
+                                <input type="checkbox" name="swich-theme" id="themeSwitch" onClick={this.changeTheme} defaultChecked />
+                                <div className="toggle-bg"></div>
+                                <div className="toggle-thumb">
+                                    <i className="fas fa-sun"></i>
+                                    <i className="fas fa-moon"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="poke__logos noselect">
+                            <img src={Pokedex} alt="pokelogo" className="poke__logo" />
+                        </div>
+                        <div className="pokeball__box github__icon" onClick={this.openGithub}>
+                            <GitHubIcon></GitHubIcon>
+                        </div>
+                    </div> */}
+            <Header />
             <div className="filter__container noselect">
               <div className="filter__items">
                 <div>Region</div>
